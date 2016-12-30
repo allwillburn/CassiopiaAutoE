a0819e0469dd1f4e7005f4232f71491a45d34771
allwillburn <livefortoday470@gmail.com>
2016-12-29 21:34:10 -0500

    Update CassiopiaAutoE.lua

--- CassiopiaAutoE.lua
+++ CassiopiaAutoE.lua
@@ -9,13 +9,13 @@ OnTick(function (myHero)
   
 local BonusAP = GetBonusDmg(myHero)
 local BaseAP = GetBaseDamage(myHero)
-local EDmg = 1.6 * GetCastLevel(myHero, _E) + (BaseAP + BonusAP) * 1.
+local EDmg = 1.8 * GetCastLevel(myHero, _E) + (BaseAP + BonusAP) * 1.
 local ERange = 700
 
  
 BonusAP = GetBonusDmg(myHero)
 BaseAP = GetBaseDamage(myHero)
-EDmg = 1.6 * GetCastLevel(myHero, _E) + (BaseAP + BonusAP) * 1.
+EDmg = 1.8 * GetCastLevel(myHero, _E) + (BaseAP + BonusAP) * 1.
 
 --Auto E on minions
     for _, minion in pairs(minionManager.objects) do
